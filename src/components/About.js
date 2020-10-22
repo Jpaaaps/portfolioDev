c4f4bfcd7a608a056f014992b366a3e50619b45b
jpaaaps <jpgerard87@gmail.com>
2020-10-22 11:25:41 +0200

    correction of spelling mistakes

--- src/components/About.js
+++ src/components/About.js
@@ -24,11 +24,11 @@ function handleClick(e){
 					<div className='profile'>
 
 						<img className='imgPro' src={photo} alt='moi'/>
-						<p className='card'>Responsable logistique et musicien autodidacte acharné, je poursuis mon parcours dans le domaine du développement web depuis le début d'année 2020 dans le cadre d'une reconversion professionnelle. J'ai eu la chance de suivre une formation de 5 mois à la <span><a href='https://www.wildcodeschool.com/fr-FR' target='_blank' rel="noopener noreferrer">Wild Code School</a></span>, et en parallèle diverses formations sur des sites tels que <span><a href='https://www.udemy.com' target='_blank' rel="noopener noreferrer">Udemy</a></span> pour approfondire mes connaissances. 
+						<p className='card'>Responsable logistique et musicien autodidacte acharné, je poursuis mon parcours dans le domaine du développement web depuis le début d'année 2020 dans le cadre d'une reconversion professionnelle. J'ai eu la chance de suivre une formation de 5 mois à la <span><a href='https://www.wildcodeschool.com/fr-FR' target='_blank' rel="noopener noreferrer">Wild Code School</a></span>, et en parallèle diverses formations sur des sites tels que <span><a href='https://www.udemy.com' target='_blank' rel="noopener noreferrer">Udemy</a></span> pour approfondir mes connaissances. 
 						Je suis désormais Développeur Web basé à PARIS. 
 						<Link to='/contact'><p className='linkTo'>Déjà envie de me contacter?</p></Link>
 						</p>
-						
+
 					</div>
 
 					<button onClick={handleClick}>Voir mon CV</button>
